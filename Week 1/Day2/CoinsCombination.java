73a7fb14d14d5bb733c2a098c92885c59fb685ab
uday164 <udaykarthik.karthik96@gmail.com>
2018-06-28 09:13:07 +0530

    Update CoinsCombination.java

--- Week 1/Day2/CoinsCombination.java
+++ Week 1/Day2/CoinsCombination.java
@@ -1,1 +1,37 @@
 
+import java.util.*;
+
+class CoinsCombination
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int arr[] =  new int[n];
+
+		for (int i = 0;i<n ;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+
+		int x = sc.nextInt();
+
+		int arr1[] = new int[x+1];
+
+		arr1[0] = 1;
+
+		for (int j = 0;j<n;j++)
+		{
+			for (int k = 0;k<x+1;k++)
+			{
+				if (k>=arr[j])
+				{
+					arr1[k] += arr1[k - arr[j]];
+				}	
+			}
+		}
+		System.out.println(arr1[x]);	
+	}
+}

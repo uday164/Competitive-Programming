ca49fac6bf1d330eef92cbd748a4c45c000e8943
uday164 <udaykarthik.karthik96@gmail.com>
2018-06-28 14:11:36 +0530

    Update SecLargestBT.java

--- Week 1/Day 4/SecLargestBT.java
+++ Week 1/Day 4/SecLargestBT.java
@@ -1,1 +1,83 @@
+import java.util.*;
 
+class Node
+{
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data)
+	{
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+
+class SecLargestBT
+{
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+
+	Node root;
+
+	SecLargestBT(int key)
+	{
+		root = new Node(key);
+	}
+
+	SecLargestBT()
+	{
+		root = null;
+	}
+
+	 void printInorder()
+	 {
+	      printInorder(root);   
+	 }
+
+	  void printInorder(Node node)
+    {
+        if (node == null)
+            return;
+ 
+        printInorder(node.left);
+
+        arr.add(node.data);
+
+
+ 
+        printInorder(node.right);
+    }
+
+    void SecLargest()
+    {
+    	System.out.println(arr.get(arr.size()-2));
+    }
+
+
+	public static void main(String[] args)
+	{
+		SecLargestBT tree = new SecLargestBT();
+
+		tree.root = new Node(4);
+
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(8);
+
+		tree.root.left.left = new Node(1);
+		tree.root.left.right = new Node(3);
+
+	    tree.root.right.left = new Node(6);
+		tree.root.right.right = new Node(11);
+
+
+		tree.printInorder();
+
+		tree.SecLargest();
+
+
+	}
+
+
+}

489352e8d560f419f66544c57013de5060db5a39
uday164 <udaykarthik.karthik96@gmail.com>
2018-07-14 14:43:30 +0530

    Update Hamming.java

--- WEEK3 EXAM/Hamming.java
+++ WEEK3 EXAM/Hamming.java
@@ -1,1 +1,52 @@
 
+import java.util.*;
+
+class Hamming
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		String str = Integer.toBinaryString(a);
+		String str1 = Integer.toBinaryString(b);
+
+		if (str.length() < str1.length())
+		{
+			int j = str1.length() - str.length();
+
+			for(int i = 0;i<j;i++)
+			{
+				str = "0" + str;
+			}
+			
+		}
+
+		else if(str.length() > str1.length())
+		{
+			int j = str.length() - str1.length();
+
+			for(int i = 0;i<j;i++)
+			{
+				str1 = "0" + str1;
+			}
+
+		}
+
+        int count =0;
+		for (int k = 0;k<str.length();k++)
+		{
+			if (str.charAt(k)!=str1.charAt(k))
+			{
+				count++;
+			}
+			
+		}
+
+		System.out.println(count);
+
+		
+	}
+}
